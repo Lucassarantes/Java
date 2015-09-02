ecb28fabf4d532d552a7db6d6518510236bd4c30
Lucas <lucas.sarantes@gmail.com>
2015-09-02 19:07:41 -0300

    Adicionei os detalhes dos exercicios do cap 5

--- Fontes/Exercicios_Cap_4_5_ApCaelum/ExerciciosFuncionario/Empresa.java
+++ Fontes/Exercicios_Cap_4_5_ApCaelum/ExerciciosFuncionario/Empresa.java
@@ -1,1 +1,32 @@
-class Empresa
+class Empresa{
+	String nomeDaEmpresa;
+	String cnpj;
+	Funcionario[] funcionario = new Funcionario[10];
+	int contador = 0;
+
+	void setNomeDaEmpresa(String nomeDaEmpresa){
+		this.nomeDaEmpresa = nomeDaEmpresa;
+	}
+	String getNomeDaEmpresa(){
+		return this.nomeDaEmpresa;
+	}
+
+	void setCnpj(String cnpj){
+		this.cnpj = cnpj;
+	}	
+	String getCnpj(){
+		return this.cnpj;
+	}
+
+	void adicionaFuncionario(Funcionario funcionario){
+		this.funcionario[contador] = funcionario;
+		contador += 1;
+	}
+	void mostraFuncionarios(){
+		for (int i = 0; i < contador; i++) {
+			System.out.println(" Informações do funcionário: \n");
+			funcionario[i].mostra();
+			System.out.println(" Funcionário na posição: "+i);
+		}
+	}
+}
---
+++ Fontes/Exercicios_Cap_4_5_ApCaelum/ExerciciosFuncionario/TestaEmpresa.java
@@ -0,0 +1,24 @@
+class TestaEmpresa{
+	public static void main(String[] args) {
+		Empresa empresa = new Empresa();
+		Funcionario funcionario1 = new Funcionario();
+		Funcionario funcionario2 = new Funcionario();
+		funcionario1.dataDeEntrada = new Data();
+		funcionario2.dataDeEntrada = new Data();
+
+		funcionario1.setNomeDoFuncionario("Lucas");
+		funcionario1.dataDeEntrada.setDia(31);
+		funcionario1.dataDeEntrada.setMes(8);
+		funcionario1.dataDeEntrada.setAno(2015);
+
+		funcionario2.setNomeDoFuncionario("Zé");
+		funcionario2.dataDeEntrada.setDia(31);
+		funcionario2.dataDeEntrada.setMes(8);
+		funcionario2.dataDeEntrada.setAno(2015);
+
+		empresa.adicionaFuncionario(funcionario1);
+		empresa.adicionaFuncionario(funcionario2);
+
+		empresa.mostraFuncionarios();
+	}
+}
